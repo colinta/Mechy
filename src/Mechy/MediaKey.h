7c4572916c15fa9c62e120095ea15dd0f328ac98
Colin T.A. Gray <colinta@gmail.com>
2018-11-13 12:26:19 -0500

    add the rest of supported keys to MediaKey

--- src/Mechy/MediaKey.h
+++ src/Mechy/MediaKey.h
@@ -13,6 +13,12 @@ protected:
 };
 
 #define MD(k) { .name = FN_MEDIA, .key = k }
-#define MD_VOLU MD(MEDIA_VOL_UP)
-#define MD_VOLD MD(MEDIA_VOL_DOWN)
-#define MD_PLAY MD(MEDIA_PLAY)
+#define MD_VOLU  MD(MEDIA_VOL_UP)
+#define MD_VOLD  MD(MEDIA_VOL_DOWN)
+#define MD_PLAY  MD(MEDIA_PLAY)
+#define MD_PAUSE MD(MEDIA_PAUSE)
+#define MD_STOP  MD(MEDIA_STOP)
+#define MD_NEXT  MD(MEDIA_NEXT)
+#define MD_PREV  MD(MEDIA_PREV)
+#define MD_FFD   MD(MEDIA_FFD)
+#define MD_RWD   MD(MEDIA_RWD)
