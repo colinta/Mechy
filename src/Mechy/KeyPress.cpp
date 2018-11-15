d7595d2b256b423185fc72b1f22c02fc65d6bb97
Colin T.A. Gray <colinta@gmail.com>
2018-11-15 09:31:27 -0500

    shorter mod compare code, don't need local var

--- src/Mechy/KeyPress.cpp
+++ src/Mechy/KeyPress.cpp
@@ -9,22 +9,20 @@ uint8_t KeyPress::defaultName() {
 }
 
 void KeyPress::run(Event* event) {
-    uint16_t mods = (event->key & (MOD_LSFT | MOD_LGUI | MOD_LCTL | MOD_LALT));
-
     if (event->isPressed()) {
-        if (mods & MOD_LSFT) { mechy->sendKeyboardPress(KEY_LEFT_SHIFT); }
-        if (mods & MOD_LGUI) { mechy->sendKeyboardPress(KEY_LEFT_GUI); }
-        if (mods & MOD_LCTL) { mechy->sendKeyboardPress(KEY_LEFT_CTRL); }
-        if (mods & MOD_LALT) { mechy->sendKeyboardPress(KEY_LEFT_ALT); }
+        if (event->key & MOD_LSFT) { mechy->sendKeyboardPress(KEY_LEFT_SHIFT); }
+        if (event->key & MOD_LGUI) { mechy->sendKeyboardPress(KEY_LEFT_GUI); }
+        if (event->key & MOD_LCTL) { mechy->sendKeyboardPress(KEY_LEFT_CTRL); }
+        if (event->key & MOD_LALT) { mechy->sendKeyboardPress(KEY_LEFT_ALT); }
 
         mechy->sendKeyboardPress(event->key & 0b11111111);
     }
     else if (event->isReleased()) {
         mechy->sendKeyboardRelease(event->key & 0b11111111);
 
-        if (mods & MOD_LSFT) { mechy->sendKeyboardRelease(KEY_LEFT_SHIFT); }
-        if (mods & MOD_LGUI) { mechy->sendKeyboardRelease(KEY_LEFT_GUI); }
-        if (mods & MOD_LCTL) { mechy->sendKeyboardRelease(KEY_LEFT_CTRL); }
-        if (mods & MOD_LALT) { mechy->sendKeyboardRelease(KEY_LEFT_ALT); }
+        if (event->key & MOD_LSFT) { mechy->sendKeyboardRelease(KEY_LEFT_SHIFT); }
+        if (event->key & MOD_LGUI) { mechy->sendKeyboardRelease(KEY_LEFT_GUI); }
+        if (event->key & MOD_LCTL) { mechy->sendKeyboardRelease(KEY_LEFT_CTRL); }
+        if (event->key & MOD_LALT) { mechy->sendKeyboardRelease(KEY_LEFT_ALT); }
     }
 }
